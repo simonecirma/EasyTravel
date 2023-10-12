977592d820376ee526fde306a0ffbcfae14f8194
simonecirma <s.cirma@studenti.unisa.it>
2023-10-12 11:12:18 +0200

    Create Project

---
+++ Progetto/src/model/ProductModel.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class ProductModel {
+	
+	private static DataSource ds;
+
+	static {
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+			ds = (DataSource) envCtx.lookup("jdbc/easytravel");
+
+		} catch (NamingException e) {
+			System.out.println("Error:" + e.getMessage());
+		}
+	}
+	
+	private static final String TABLE_NAME_PACCHETTO = "Pacchetto";
+	
+	
+}
